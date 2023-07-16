f765cf2104ab7988208dbbe1161ecd69e3cb240e
V-Malar <128116398+V-Malar@users.noreply.github.com>
2023-07-16 10:04:06 -0700

    Create Closed17b.java

---
+++ remaining_practice/Closed17b.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.Scanner;
+
+public class Closed17b {
+	static int a = 10;
+
+	void display() {
+		System.out.println(a);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Closed13 c = new Closed13();
+		c.display();
+	}
+}
