1ec1b3920fb8fb53d52dc94bfcc966bcf593135e
V-Malar <128116398+V-Malar@users.noreply.github.com>
2023-07-17 02:32:44 -0700

    Add files via upload

---
+++ remaining_practice/B.java
@@ -0,0 +1,21 @@
+class A
+{
+public static void main(String args[])
+{
+System.out.println("Hello");
+}
+}
+
+//OUTPUT
+
+Microsoft Windows [Version 10.0.19045.3208]
+(c) Microsoft Corporation. All rights reserved.
+
+C:\Users\DELL>d:
+
+D:\>cd src
+
+D:\src>javac B.java
+
+D:\src>java A
+Hello
