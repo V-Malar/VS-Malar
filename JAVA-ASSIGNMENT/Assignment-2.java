9d9a3bad409a9da5e1198c01b2b88bb80ac18f76
V-Malar <128116398+V-Malar@users.noreply.github.com>
2023-07-31 09:59:57 -0700

    Update Assignment-2.java

--- JAVA-ASSIGNMENT/Assignment-2.java
+++ JAVA-ASSIGNMENT/Assignment-2.java
@@ -1,1 +1,41 @@
+package exeption;
 
+import java.util.Scanner;
+
+class Rahul {
+	Scanner sc = new Scanner(System.in);
+	Integer n, count = 0, s = 1;
+	Integer[] i;
+
+	public void calculate() {
+		try {
+			System.out.println("Enter the number count: ");
+			n = sc.nextInt();
+			i = new Integer[n];
+			System.out.println("Even number divisible by 3");
+			for (int l = 0; l < n; l++) {
+				if (l % 2 == 0) {
+					s = l;
+					if (s % 3 == 0) {
+						System.out.println(s + "*");
+						count = count + s;
+					}
+				}
+			}
+			System.out.println("Count: " + count);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+
+public class Work {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rahul r = new Rahul();
+		r.calculate();
+	}
+
+}
