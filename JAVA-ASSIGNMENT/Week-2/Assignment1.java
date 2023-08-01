cf3b81499d874ed35e7c825eca6cb2eb134e59cf
V-Malar <128116398+V-Malar@users.noreply.github.com>
2023-08-01 11:39:43 +0530

    Create Assignment1.java

---
+++ JAVA-ASSIGNMENT/Week-2/Assignment1.java
@@ -0,0 +1,23 @@
+package Week2;
+
+class School {
+	public static void print() {
+		System.out.println("Hi! I class SCHOOL.");
+	}
+}
+
+class Student {
+	public static void print() {
+		System.out.println("Hi! I am class STUDENT");
+	}
+}
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		School.print();
+		Student.print();
+	}
+
+}
