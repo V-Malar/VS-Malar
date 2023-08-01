f43e7a724af4a5cbfc019ac0ece7ac65259ac6ed
V-Malar <128116398+V-Malar@users.noreply.github.com>
2023-08-01 12:33:28 +0530

    Update Assignment3.java

--- JAVA-ASSIGNMENT/Week-2/Assignment3.java
+++ JAVA-ASSIGNMENT/Week-2/Assignment3.java
@@ -1,21 +1,36 @@
 package Week2;
 
-class Question
-{
-	public void student() {
-		print(this); // Replaced for new Question()
+import java.util.Scanner;
+
+class AnotherQuestion {
+	public static void print(Question q) {
+		System.out.println("Well Done! " + q.getClass().getSimpleName());
 	}
-	public static void print(Question q)
-	{
+}
+class OneMoreQuestion
+{
+	public static void print(Question q) {
+		System.out.println("Well Done! " + q.getClass().getSimpleName());
+	}
+}
+class Question {
+	public static void student() throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the input class: ");
+		String input_class_name = sc.next();
+		print(Class.forName(input_class_name ).newInstance());
+	}
+
+	public static void print(Object q) {
 		System.out.println("Well Done! " + q.getClass().getSimpleName());
 	}
 }
 
-public class Assignment3 {
+public class Assignment1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
 		// TODO Auto-generated method stub
-		new Question().student();
+		Question.student();
 	}
 
 }
