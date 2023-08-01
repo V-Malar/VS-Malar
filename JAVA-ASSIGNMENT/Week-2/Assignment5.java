8e0b9a8d344023434cb21eb4d99777236af8cc8e
V-Malar <128116398+V-Malar@users.noreply.github.com>
2023-08-01 12:42:52 +0530

    Create Assignment5.java

---
+++ JAVA-ASSIGNMENT/Week-2/Assignment5.java
@@ -0,0 +1,21 @@
+package Week2;
+
+class Question {
+
+}
+
+public class Assignment5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Declare variable with name 'nptel', 'space' and 'java' and proper datatype
+		String nptel, space, java;
+		// Initialize the variables with proper input
+		nptel = "NPTEL";
+		space = " ";
+		java = "JAVA";
+
+		System.out.print(nptel + space + java + space.replace(" ", "") + nptel.replace("NPTEL",  ""));
+	}
+
+}
