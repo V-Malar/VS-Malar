b14c1e65b41a774c34c4bbec3bd5c14c9ac8afef
V-Malar <128116398+V-Malar@users.noreply.github.com>
2023-08-05 23:17:19 +0530

    Update Assignment5.java

--- JAVA-ASSIGNMENT/Week-5/Assignment5.java
+++ JAVA-ASSIGNMENT/Week-5/Assignment5.java
@@ -1,39 +1,42 @@
-package Week2;
+package Week;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import java.util.Scanner;
+class Do {
+	static Scanner sc = new Scanner(System.in);
 
-public class Assignment5 {
-	// Function to calculate the GCD using the Euclidean Algorithm
-	public static int calculateGCD(int a, int b) {
-		if (a == 0 && b == 0) {
-			return -1;
-		} else if (b == 0) {
-			return a;
-		} else if (a == b) {
-			return 1;
-		} else if (a % b == 0) {
-			return b;
-		} else {
-			if (a % 2 == 0 || b % 2 == 0) {
-				return 1;
-			} else {
-				return a;
+	public static void GCD() {
+		Integer n1 = 0, n2 = 0;
+		boolean step1 = false, step2 = false;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter n1 value: ");
+		while (!step1) {
+			try {
+				n1 = sc.nextInt();
+				step1 = true;
+			} catch (InputMismatchException e) {
+				sc.next();
 			}
 		}
+		System.out.println("Enter n2 value: ");
+		while (!step2) {
+			try {
+				n2 = sc.nextInt();
+				step2 = true;
+			} catch (InputMismatchException e) {
+				sc.next();
+			}
+		}
+		while (n2 != 0) {
+			int temp = n2;
+			n2 = n1 % n2;
+			n1 = temp;
+		}
+		System.out.println("GCD: " + n1);
 	}
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		System.out.print("Enter the first number: ");
-		int num1 = sc.nextInt();
-
-		System.out.print("Enter the second number: ");
-		int num2 = sc.nextInt();
-
-		int gcd = calculateGCD(num1, num2);
-		System.out.println("The GCD of " + num1 + " and " + num2 + " is: " + gcd);
+		GCD();
 	}
 }
