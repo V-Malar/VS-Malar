707984f080f864dc94380c1fcbda5ed05ac7b9ff
V-Malar <128116398+V-Malar@users.noreply.github.com>
2023-08-10 10:38:47 -0700

    Update BasicGenerics.java

--- BasicGenerics/BasicGenerics.java
+++ BasicGenerics/BasicGenerics.java
@@ -1,5 +1,5 @@
 package Week;
-
+// Method GENERICS
 public class Work {
 	public static <T> void display(T[] arr) {
 		for (T i : arr) {
@@ -20,6 +20,77 @@ public class Work {
 		display(char_arr);
 		System.out.println();
 		display(str_arr);
+	}
+
+}
+// Class GENERICS
+package Week;
+
+class Shinchano {
+	String name;
+
+	public Shinchano(String name) {
+		this.name = name;
+	}
+}
+
+class Kazama {
+	Integer age;
+
+	public Kazama(Integer age) {
+		this.age = age;
+	}
+}
+
+class Masaw {
+	Character initial;
+
+	public Masaw(Character initial) {
+		this.initial = initial;
+	}
+}
+
+class Neni {
+	Boolean isFriend;
+
+	public Neni(Boolean isFriend) {
+		this.isFriend = isFriend;
+	}
+}
+
+class Shiro {
+	String type;
+
+	public Shiro(String type) {
+		this.type = type;
+	}
+}
+
+class BasicGenerics<T> {
+	T things;
+
+	public BasicGenerics(T things) {
+		this.things = things;
+	}
+
+	public T getValue() {
+		return things;
+	}
+}
+
+public class Work {
+
+	public static void main(String[] args) {
+		BasicGenerics<Shinchano> shinchan = new BasicGenerics("Shinchan");
+		System.out.println(shinchan.getValue());
+		BasicGenerics<Kazama> kazama = new BasicGenerics(5);
+		System.out.println(kazama.getValue());
+		BasicGenerics<Masaw> masaw = new BasicGenerics("M");
+		System.out.println(masaw.getValue());
+		BasicGenerics<Neni> neni = new BasicGenerics(true);
+		System.out.println(neni.getValue());
+		BasicGenerics<Shiro> shiro = new BasicGenerics("Pet");
+		System.out.println(shiro.getValue());
 	}
 
 }
