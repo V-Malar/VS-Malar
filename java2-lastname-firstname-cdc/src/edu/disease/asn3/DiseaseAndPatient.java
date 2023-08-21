372730cc82e0d5a51ddcebef854e8b87e91413a3
V-Malar <128116398+V-Malar@users.noreply.github.com>
2023-08-21 11:44:59 -0700

    Update DiseaseAndPatient.java

--- java2-lastname-firstname-cdc/src/edu/disease/asn3/DiseaseAndPatient.java
+++ java2-lastname-firstname-cdc/src/edu/disease/asn3/DiseaseAndPatient.java
@@ -1,5 +1,7 @@
 package src;
-
+/**
+ * A class representing diseases and patients.
+ */
 public class DiseaseAndPatient {
 
 	private Disease[] diseases;
@@ -8,15 +10,27 @@ public class DiseaseAndPatient {
 	public Disease[] getDiseases() {
 		return diseases;
 	}
-
+/**
+     * Get an array of diseases.
+     *
+     * @return An array of diseases.
+     */
 	public void setDiseases(Object object) {
 		this.diseases = (Disease[]) object;
 	}
-
+ /**
+     * Set the array of patients.
+     *
+     * @param patients An array of patients to set.
+     */
 	public void setPatients(Object object) {
 		this.patients = (Patient[]) object;
 	}
-
+ /**
+     * Get an array of patients.
+     *
+     * @return An array of patients.
+     */
 	public Patient[] getPatients() {
 		return patients;
 	}
