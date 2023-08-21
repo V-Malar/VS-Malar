48db2e313b477fcb470860ea7a539108fa78c13e
V-Malar <128116398+V-Malar@users.noreply.github.com>
2023-08-21 14:30:55 +0530

    Update Disease.java

--- java2-lastname-firstname-cdc/src/edu/disease/asn1/Disease.java
+++ java2-lastname-firstname-cdc/src/edu/disease/asn1/Disease.java
@@ -1,29 +1,54 @@
-package edu.disease.asn1;
-import java.util.UUID;
-
+/**
+ * The Disease class represents a disease with a unique disease ID and a name.
+ */
 public class Disease {
 
-	UUID diseaseId;
-	String name;
-	void setName(String name)
-	{
-		this.name = name;
-	}
-	String getName()
-	{
-		return name;
-	}
-	void setdiseaseId(UUID id)
-	{
-		this.diseaseId = id;
-	}
-	UUID getdiseaseId()
-	{
-		return diseaseId;
-	}
-	public String toString()
-	{
-		return "\nName: " + getName() + " Disease-ID: "+ diseaseId.hashCode();
-		
-	}
+    private UUID diseaseId;
+    private String name;
+
+    /**
+     * Sets the name of the disease.
+     *
+     * @param name The name of the disease.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Retrieves the name of the disease.
+     *
+     * @return The name of the disease.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the disease ID.
+     *
+     * @param id The disease ID.
+     */
+    public void setDiseaseId(UUID id) {
+        this.diseaseId = id;
+    }
+
+    /**
+     * Retrieves the disease ID.
+     *
+     * @return The disease ID.
+     */
+    public UUID getDiseaseId() {
+        return diseaseId;
+    }
+
+    /**
+     * Returns a string representation of the Disease object.
+     *
+     * @return A string containing the disease's name and ID.
+     */
+    @Override
+    public String toString() {
+        return "\nName: " + getName() + " Disease-ID: " + diseaseId.hashCode();
+    }
 }
