76731d8b2910bac89c6bd22db609545e53aea81d
V-Malar <128116398+V-Malar@users.noreply.github.com>
2023-08-21 19:54:29 +0530

    Update DiseaseFileRepository.java

--- java2-lastname-firstname-cdc/src/edu/disease/DiseaseFileRepository.java
+++ java2-lastname-firstname-cdc/src/edu/disease/DiseaseFileRepository.java
@@ -1,23 +1,32 @@
 package edu.disease.asn3;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class DiseaseFileRepository {
-public void save(Disease[] diseases, Patient[] patients)
-{
-//	FileOutputStream fos=new FileOutputStream("myladdu.dat");
-//	ObjectOutputStream oos=new ObjectOutputStream(fos);
-//	oos.writeObject(laddu);//saves the laddu in serialized form (in a file)....
-//	FileOutputStream fos=new FileOutputStream("myladdu.dat");
-//	ObjectOutputStream oos=new ObjectOutputStream(fos);
-//	oos.writeObject(laddu);//saves the laddu in serialized form (in a file)....
-}
-public DiseaseAndPatient init(String folderPath)
-{
-	folderPath="";
-		
-	return null;
-	
-}
+	public void save(Disease[] diseases, Patient[] patients) throws IOException {
+		String folderPath = "D:\\CPS\\LWUG\\workspace\\java2-lastname-firstname-cdc\\src\\edu\\disease\\asn3";
+		FileOutputStream fos = new FileOutputStream(folderPath + "patients.dat");
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		oos.writeObject(diseases);
+		fos = new FileOutputStream(folderPath + "diseases.dat");
+		oos = new ObjectOutputStream(fos);
+		oos.writeObject(patients);
+	}
+
+	public DiseaseAndPatient init(String folderPath) throws IOException, ClassNotFoundException {
+		folderPath = "D:\\CPS\\LWUG\\workspace\\java2-lastname-firstname-cdc\\src\\edu\\disease\\asn3";
+		FileInputStream fis=new FileInputStream(folderPath+"patients.dat");
+		ObjectInputStream ois=new ObjectInputStream(fis);
+		DiseaseAndPatient patients = (DiseaseAndPatient) ois.readObject();
+		fis = new FileInputStream(folderPath+"diseases.dat");
+		ois=new ObjectInputStream(fis);
+		DiseaseAndPatient diseases = (DiseaseAndPatient) ois.readObject();
+		return patients;
+
+	}
 }
