adfb509ba139192fa35f6f099f63732140f734e8
V-Malar <128116398+V-Malar@users.noreply.github.com>
2023-08-22 17:12:35 +0530

    Update Exposure.java

--- java2-lastname-firstname-cdc/src/edu/disease/asn1/Exposure.java
+++ java2-lastname-firstname-cdc/src/edu/disease/asn1/Exposure.java
@@ -11,25 +11,40 @@ public class Exposure {
 
 	public Exposure(UUID patientId) {
 		setPatientId(patientId);
+
 	}
 
-    public String getExposure() {
+//	public void setExposure(String exposureType) {
+//		if (!Pattern.matches("[DI]", exposureType)) {
+//			throw new IllegalArgumentException("Value must be either D or I");
+//		} else {
+//			this.exposureType = exposureType;
+//		}
+//	}
+
+	public String getExposure() {
 		return exposureType;
 	}
 
 	public String toString() {
-		// PatientID: 123e4567-e89b-12d3-a456-426655440000 Exposure Type: I Date and
-		// Time: " + dateTime
-		return "PatientID: " + patientId.hashCode() + " Exposure Type: " + getExposure() + " Date and Time: "
-				+ dateTime;
+		//PatientID: 123e4567-e89b-12d3-a456-426655440000 Exposure Type: I Date and Time: " + dateTime
+		return "PatientID: " + patientId.hashCode() + " Exposure Type: " + getExposure() + " Date and Time: " + dateTime;
 	}
 
 	public void setDateTime(LocalDateTime dateTime) {
-		this.dateTime = dateTime;
-
+		if (dateTime !=null)
+		{
+			this.dateTime = dateTime;
+		}
+		else
+		{
+			throw new NullPointerException("Null value found for Date and Time");
+		}
+		
+		
 	}
-
-	public LocalDateTime getDateTime() {
+	public LocalDateTime getDateTime()
+	{
 		return dateTime;
 	}
 
@@ -46,12 +61,15 @@ public class Exposure {
 	}
 
 	public void setPatientId(UUID patientId) {
-		if (patientId != null) {
-			this.patientId = patientId;
-		} else {
-			throw new IllegalArgumentException("Null value found for ID");
+		if (patientId !=null)
+		{
+		this.patientId = patientId;
 		}
-
+		else
+		{
+			throw new NullPointerException("Null value found for ID");
+		}
+		
 	}
 
 	public String getExposureType() {
