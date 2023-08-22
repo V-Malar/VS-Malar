a16481f520b6a095ef61a6789864863773fe3bcf
V-Malar <128116398+V-Malar@users.noreply.github.com>
2023-08-22 17:12:12 +0530

    Update ExposureTest.java

--- java2-lastname-firstname-cdc/test/edu/disease/asn1/ExposureTest.java
+++ java2-lastname-firstname-cdc/test/edu/disease/asn1/ExposureTest.java
@@ -1,46 +1,62 @@
 package edu.disease.asn1;
 
-import org.junit.Test;
-
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.time.LocalDateTime;
 import java.util.UUID;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.Test;
 
 public class ExposureTest {
+	private Exposure exposure;
 
-    @Test
-    public void testValidExposureType() {
-        LocalDateTime dateTime = LocalDateTime.now();
-        UUID patientId = UUID.randomUUID();
+	@Before
+	public void setUp() {
+		final UUID uuid = UUID.randomUUID();
+		exposure = new Exposure(uuid);
+		exposure.setDateTime(LocalDateTime.now());
+		exposure.setExposureType("D");
+	}
 
-        Exposure exposure = new Exposure("D", dateTime, patientId);
+	@Test
+	public void testExposurePostiveScenario() {
+		assertNotNull(exposure.toString());
+		assertNotNull(exposure.getDateTime());
+		assertNotNull(exposure.getPatientId());
+		assertEquals("D", exposure.getExposureType());
+		assertNotNull(exposure.hashCode());
+		final Exposure exposureObj = exposure;
+		assertTrue(exposure.equals(exposureObj));
+		assertTrue(exposure.equals(exposure));
+		assertFalse(exposure.equals(null));
+		final Exposure exposureObjEquals = new Exposure(exposure.getPatientId());
+		exposureObjEquals.setExposureType("I");
+		exposureObjEquals.setDateTime(exposure.getDateTime());
+		System.out.println(exposure);
+		System.out.println(exposureObjEquals);
+		assertThrows(NullPointerException.class, () -> exposure.setPatientId(null));
+        assertThrows(NullPointerException.class, () -> exposureObjEquals.setDateTime(null));
+		assertFalse(exposure.equals(exposureObjEquals));
+		assertFalse(exposure.equals(null));
+		assertFalse(exposure.equals(exposureObjEquals));
+		exposure.setPatientId(exposure.getPatientId());
+		exposureObjEquals.setDateTime(exposure.getDateTime());
+		System.out.println(exposure);
+		System.out.println(exposureObjEquals);
+		assertFalse(exposure.equals(exposureObjEquals));
+		
+	}
 
-        assertEquals("D", exposure.getExposure());
-    }
-
-    @Test
+	@Test
     public void testInvalidExposureType() {
-        LocalDateTime dateTime = LocalDateTime.now();
-        UUID patientId = UUID.randomUUID();
-
-        try {
-            Exposure exposure = new Exposure("X", dateTime, patientId);
-            fail("Expected IllegalArgumentException was not thrown");
-        } catch (IllegalArgumentException e) {
-            // Expected exception
-        }
+        Exposure exposure = new Exposure(UUID.randomUUID());
+        
+        // Set an invalid exposure type, expecting an exception
+        assertThrows(IllegalArgumentException.class, () -> exposure.setExposureType("X"));
+        assertThrows(IllegalArgumentException.class, () -> exposure.setExposureType("Z"));
     }
-
-    @Test
-    public void testExposureToString() {
-        LocalDateTime dateTime = LocalDateTime.now();
-        UUID patientId = UUID.fromString("123e4567-e89b-12d3-a456-426655440000");
-
-        Exposure exposure = new Exposure("I", dateTime, patientId);
-
-        String expectedString = "PatientID: " + patientId.hashCode() + " Exposure Type: I Date and Time: " + dateTime;
-        assertEquals(expectedString, exposure.toString());
-    }
+	
 }
