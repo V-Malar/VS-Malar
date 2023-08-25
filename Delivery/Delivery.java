8233d43f0340d83010b894d2e92bfbe117dd4efe
V-Malar <128116398+V-Malar@users.noreply.github.com>
2023-08-25 18:32:52 +0530

    Update Delivery.java

--- Delivery/Delivery.java
+++ Delivery/Delivery.java
@@ -1,3 +1,4 @@
+package twentythree;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -50,76 +51,73 @@ public class Delivery {
 		keyValuePairs.add(new KeyValue("NP", 2));
 		keyValuePairs.add(new KeyValue("NP", 8));
 		keyValuePairs.add(new KeyValue("NP", 10));
-		keyValuePairs.add(new KeyValue("NP", 2));
+		keyValuePairs.add(new KeyValue("NP", 4));
 		newDateTime = parsedDateTime.plusHours(result);
 		day_formatter = DateTimeFormatter.ofPattern("EEEE");
 		String dayOfWeek = newDateTime.format(day_formatter);
 		String formattedDate = newDateTime.format(outputFormatter);
 		System.out.println(checkTime.getHour());
+		System.out.println("*******" + parsedDateTime.plusHours(24) + " " + newDateTime.getDayOfWeek());
+
 		if (dayOfWeek.equalsIgnoreCase("Saturday") || dayOfWeek.equalsIgnoreCase("Sunday")
 				|| formattedCheckTime.equalsIgnoreCase("11:30:00 PM")) {
 			System.out.println("Destination stopped..");
-		}
-		else if(dayOfWeek.equalsIgnoreCase("Friday") && (checkTime.getHour() >= 20))
-		{
+		} else if (dayOfWeek.equalsIgnoreCase("Friday") && (checkTime.getHour() >= 20)) {
 			System.out.println("Destination stopped.." + newDateTime);
-		}
-		else { if (formattedDate.equalsIgnoreCase("August 15, 2023")
-				|| formattedDate.equalsIgnoreCase("January 01, 2023")) {
-			if (parsedDateTime.getHour() < 24) {
-				System.out.println(newDateTime.getHour() + " Your leave is still ON..");
-			} else {
-				System.out.println(newDateTime.getHour() + " Leave Over..Work starts..");
-			}
 		} else {
-			try {
-
-				{
-					for (KeyValue keyValue : keyValuePairs) {
-						result += keyValue.value;
-						newDateTime = parsedDateTime.plusHours(result);
-						System.out.println("\n\"" + keyValue.key + "\" - Travel Time: " + keyValue.value + " Hr " + newDateTime.format(day_formatter));
-						formattedDate = newDateTime.format(outputFormatter);
-						newDateTime = parsedDateTime.plusHours(result);
-						day_formatter = DateTimeFormatter.ofPattern("EEEE");
-						dayOfWeek = newDateTime.format(day_formatter);
-
-						if (dayOfWeek.equalsIgnoreCase("Saturday")) {
-							System.out.println("Current **Day: " + newDateTime.getDayOfWeek());
-							System.out.println(newDateTime.getHour());
-							System.out.println(result);
-							newDateTime = parsedDateTime.plusHours(48+result);
-							System.out.println("Destination stopped..Delivered on: " + newDateTime.format(formatter) + " " + newDateTime.getDayOfWeek());
-							break;
-						} 
-						if (dayOfWeek.equalsIgnoreCase("Sunday"))
-						{
-							System.out.println("Current Date & Time: " + newDateTime.format(formatter) + " Day: " + newDateTime.getDayOfWeek());
-							System.out.println(newDateTime.getHour());
-							newDateTime = parsedDateTime.plusHours(result);
-						    System.out.println("Destination stopped..Delivered on: " + newDateTime.plusHours(result).format(formatter) + " " + newDateTime.plusHours(result).getDayOfWeek());
-						}
-						else {
-							System.out.println("Current Date & Time: " + newDateTime.format(formatter) + " Day: " + newDateTime.getDayOfWeek());
-							System.out.println("Destination reached..");
-						}
-						System.out.println();
-						//day_formatter = DateTimeFormatter.ofPattern("EEEE");
-						//dayOfWeek = newDateTime.format(day_formatter);August 18, 2023 09:30:00 PM
-						//+newDateTime.getHour()
-					}
-
+			String[] name = formattedDate.split(",");
+			if (name[0].equalsIgnoreCase("August 15")
+					|| name[0].equalsIgnoreCase("January 01")) {
+				if (parsedDateTime.getHour() < 24) {
+					System.out.println(newDateTime.getHour() + " Your leave is still ON..");
+				} else {
+					System.out.println(newDateTime.getHour() + " Leave Over..Work starts..");
 				}
-			}catch (Exception e) {
-				throw new DateTimeParseException("Invalid Input..", userInput, 0);
-			}}}
+			} else {
+				try {
+
+					{
+						for (KeyValue keyValue : keyValuePairs) {
+							result += keyValue.value;
+							System.out.println("SEE RESULT:" + result);
+							newDateTime = parsedDateTime.plusHours(result);
+							System.out.println("\n\"" + keyValue.key + "\" - Travel Time: " + keyValue.value + " Hr "
+									+ newDateTime.format(day_formatter));
+							formattedDate = newDateTime.format(outputFormatter);
+							newDateTime = parsedDateTime.plusHours(result);
+							day_formatter = DateTimeFormatter.ofPattern("EEEE");
+							dayOfWeek = newDateTime.format(day_formatter);
+
+							if (dayOfWeek.equalsIgnoreCase("Saturday")) {
+								System.out.println("Current **Day: " + newDateTime.getDayOfWeek() + " " + newDateTime);
+								System.out.println(newDateTime.getHour());
+								System.out.println("Add: " + result);
+								newDateTime = parsedDateTime.plusHours(48 + result);
+								System.out.println("Destination stopped..Delivered on: " + newDateTime.format(formatter)
+										+ " " + newDateTime.getDayOfWeek() + newDateTime);
+								continue;
+							} else {
+								System.out.println("Current Date & Time: " + newDateTime.format(formatter) + " Day: "
+										+ newDateTime.getDayOfWeek());
+								System.out.println("Destination reached..");
+							}
+							System.out.println();
+						}
+
+					}
+				} catch (Exception e) {
+					throw new DateTimeParseException("Invalid Input..", userInput, 0);
+				}
+			}
+		}
 	}
-/**
- * Calculates the delivery time and prints relevant information.
- *
- * @param userInput The user-input date and time in the format "MMMM d, yyyy
- *                  hh:mm:ss a".
- */
+
+	/**
+	 * Calculates the delivery time and prints relevant information.
+	 *
+	 * @param userInput The user-input date and time in the format "MMMM d, yyyy
+	 *                  hh:mm:ss a".
+	 */
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
@@ -127,22 +125,8 @@ public class Delivery {
 		String userInput = scanner.nextLine();
 		scanner.close();
 		new Delivery().deliveryTime(userInput);
-//		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("E");
-//		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEEE");
-//        DateTimeFormatter day_formatter = DateTimeFormatter.ofPattern("EEEE");
-//        DateTimeFormatter time_formatter = DateTimeFormatter.ofPattern("hh:mm:ss a");
-//        String dayOfWeek = currentDateTime.format(day_formatter);
-//        newDateTime = currentDateTime.plusHours(26);
-//        String newdayOfWeek = newDateTime.format(day_formatter);
-//        String formattedTime = currentDateTime.format(time_formatter);
-//        String newformattedTime = newDateTime.format(time_formatter);
-//        System.out.println("Current day of the week: " + dayOfWeek + " Date: " + formattedTime);
-//		System.out.println("Current day of the week: " + newdayOfWeek + " Date: " + newformattedTime);
-//		if (!dayOfWeek.equalsIgnoreCase("Saturday"))
-//		{
-//			System.out.println("Destination reached..");
-//		}
-	}}
+	}
+}
 
 
 import java.time.LocalDateTime;
